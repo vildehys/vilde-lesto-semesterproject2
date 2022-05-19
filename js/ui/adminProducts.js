3a47d65f8bb2c1e97f502463f84b3dee0cf92359
vildehys <79218980+vildehys@users.noreply.github.com>
2022-05-19 12:38:27 +0200

    updt

--- js/ui/adminProducts.js
+++ js/ui/adminProducts.js
@@ -12,7 +12,7 @@ export function adminProducts(productsForAdmin) {
                                         <h3>${product.title}</h3>
                                         <p>${product.price} kr</p>
                                         <a class="button read-button" href="edit-product.html?id=${product.id}">Edit Product</a>
-                                        <a class="button read-button" href="detail.html?id=${product.id}">Delete Product</a>
+                                        <a class="button read-button">Delete Product</a>
                                         </div
                                         </div>`;
 
